c200bbf587d365b076aa0144a738cfb38fa4be11
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2011-11-22 16:05:10 +0000

    Import of ILMARI/MooseX-Types-DateTime-0.06 from CPAN.
    gitpan-cpan-distribution: MooseX-Types-DateTime
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         ILMARI/MooseX-Types-DateTime-0.06.tar.gz
    gitpan-cpan-author:       ILMARI
    gitpan-cpan-maturity:     released

--- lib/MooseX/Types/DateTime.pm
+++ lib/MooseX/Types/DateTime.pm
@@ -5,7 +5,7 @@ package MooseX::Types::DateTime;
 use strict;
 use warnings;
 
-our $VERSION = "0.05";
+our $VERSION = "0.06";
 
 use DateTime ();
 use DateTime::Locale ();
@@ -30,9 +30,9 @@ subtype Locale,   as 'DateTime::Locale';
 subtype( Now,
     as Str,
     where { $_ eq 'now' },
-    Moose::Util::TypeConstraints::optimize_as {
-        no warnings 'uninitialized';
-        !ref($_[0]) and $_[0] eq 'now';
+    Moose::Util::TypeConstraints::inline_as {
+        'no warnings "uninitialized";'.
+        '!ref(' . $_[1] . ') and '. $_[1] .' eq "now"';
     },
 );
 
